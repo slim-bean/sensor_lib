4f2479355f7f930cbfd28b17f3a03b4a6fb8c509
Edward Welch <ed@oqqer.com>
2018-08-05 10:54:38 -0400

    adding metric information to the sensor definitions added a simple test which de-serializes the yaml file looking for syntax errors

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #[macro_use]
 extern crate serde_derive;
 
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum SensorFormat {
     Bool,
@@ -12,11 +14,22 @@ pub enum SensorFormat {
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum MetricType {
+    Gauge,
+    Counter,
+    Histogram,
+    Summary,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct SensorDefinition {
     pub id: u16,
     pub name: String,
     pub format: SensorFormat,
-    pub destination_queues: Vec<String>
+    pub destination_queues: Vec<String>,
+    pub metric_name: String,
+    pub metric_type: MetricType,
+    pub metric_labels: HashMap<String, String>,
 }
 
 
@@ -30,8 +43,17 @@ pub struct SensorValue {
 
 #[cfg(test)]
 mod tests {
+    use std::fs::File;
+    use std::io::BufReader;
+    use SensorDefinition;
+
+    extern crate serde_yaml;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn validate_yaml() {
+        let file = File::open("sensors/sensors.yml").unwrap();
+        //TODO load this into a Map rather than a Vec
+        let sensors_vec = serde_yaml::from_reader::<BufReader<File>, Vec<SensorDefinition>>(BufReader::new(file)).unwrap();
+        println!("sensors: {:?}", sensors_vec);
     }
 }
